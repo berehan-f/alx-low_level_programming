ae1b1280ab8b5f148ed02bcb3a62e2e51a1928b0
berehan-f <berehanf.hewot@gmail.com>
2023-07-18 20:14:34 +0000

    Create 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers starting with 1 & 2.
+ * Return: 0 on successful completion.
+ */
+
+int main(void)
+{
+	unsigned long tmp;
+	unsigned long i = 1;
+	unsigned long j = 2;
+	int count = 2;
+
+	printf("1, 2, ");
+	while (count < 98)
+	{
+		if (count == 97)
+			printf("%lu\n", i + j);
+		else
+			printf("%lu, ", i + j);
+		tmp = i + j;
+		i = j;
+		j = tmp;
+		count++;
+	}
+	return (0);
+}
