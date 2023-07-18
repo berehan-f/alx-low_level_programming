3d7e6ca12a9ac47c8b297613987224e865518683
berehan-f <berehanf.hewot@gmail.com>
2023-07-18 14:28:03 +0000

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - prints _putchar, followed by a new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	char word[9] = "_putchar";
+
+	for (i = 0; i < 9; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+	Return(0);
+}
