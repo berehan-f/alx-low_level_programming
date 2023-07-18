7203bf436ffb7500d030e76b0990c1bfd929c3c9
berehan-f <berehanf.hewot@gmail.com>
2023-07-18 18:09:25 +0000

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers.
+ * @x: the first addend.
+ * @y: the second addend.
+ *
+ * Return: x + y.
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
