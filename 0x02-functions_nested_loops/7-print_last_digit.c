fd480335e05f5f2bf4ef71d6e75e3419c4542475
berehan-f <berehanf.hewot@gmail.com>
2023-07-18 16:42:46 +0000

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,11 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n *= -1;
-	_putchar('0' + (n % 10));
-	return (n % 10);
+	int last_digit;
+
+	last_digit = n % 10;
+	if (last_digit < 0)
+	last_digit *= -1;
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
