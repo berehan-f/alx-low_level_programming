bf644c789bf1cff4ef9f34af7def0fd954031327
Berehan <berehanf.hewot@gmail.com>
2023-07-24 12:33:15 +0000

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line.
+ * @s: the string to be printed.
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+		i++;
+	while (i > 0)
+		_putchar(s[--i]);
+	_putchar('\n');
+}
