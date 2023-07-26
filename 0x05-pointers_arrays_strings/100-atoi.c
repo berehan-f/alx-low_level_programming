2c03f2cfda9cf2297844abb5d565f7ffa3c8bb47
Berehan <berehanf.hewot@gmail.com>
2023-07-26 04:05:46 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,27 +1,28 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
- *
- * Return: The integer value of the converted string.
+ * _atoi - Convert a string to an integer.
+ * @s: The string to convert to integer.
+ * Return: the converted integer.
  */
+
 int _atoi(char *s)
 {
+	int i, j, num = 0;
 	int sign = 1;
-	unsigned int num = 0;
 
-	do {
-		if (*s == '-')
+	for (i = 0; (s[i] < '0' || s[i] > '9') && s[i] != 0; i++)
+	{
+		printf("%c", s[i]);
+		if (s[i] == '-')
 			sign *= -1;
-
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-
-		else if (num > 0)
-			break;
-
-	} while (*s++);
-
+	}
+	printf("\n");
+	for (j = i; s[j] >= '0' && s[j] <= '9'; j++)
+	{
+		printf("%c\n", s[j]);
+		num = (num * 10) + (s[j] - '0');
+	}
 	return (num * sign);
 }
