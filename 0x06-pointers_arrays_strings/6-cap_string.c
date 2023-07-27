5a1992a601f662ab3aea51909dc798b0afc85a3f
Berehan <berehanf.hewot@gmail.com>
2023-07-27 21:02:11 +0300

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @c: the string to capitalize.
+ * Return: the capitalized string c.
+ */
+
+char *cap_string(char *c)
+{
+	int i;
+
+	for (i = 1; c[i]; i++)
+	{
+		if (!(c[i] >= 'a' && c[i] <= 'z'))
+			continue;
+		else if (i == 0)
+			c[i] -= 32;
+		else
+		{
+			switch (c[i - 1])
+			{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case '.':
+			case ';':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				c[i] -= 32;
+			default:
+				break;
+			}
+		}
+	}
+
+	return (c);
+}
