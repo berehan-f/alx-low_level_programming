ed5d21d10cb658968900bce301a0266b1497b1f6
Berehan <berehanf.hewot@gmail.com>
2023-07-27 11:10:45 +0300

    Implemet Betty coding style to 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,9 +11,9 @@ char *_strcat(char *dest, char *src)
 	int i, j, k;
 
 	for (i = 0; dest[i] != 0; i++)
-		;;
+		;
 	for (j = 0; src[j] != 0; j++)
-		;;
+		;
 	for (k = 0; k <= j; k++)
 		dest[i++] = src[k];
 	return (dest);
