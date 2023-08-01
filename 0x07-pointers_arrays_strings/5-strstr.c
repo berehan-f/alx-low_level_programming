9a450a34b0708ee0eb0e392fd7ce50b9ee787b90
Berehan <berehanf.hewot@gmail.com>
2023-08-01 04:42:28 +0300

    Create 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+/**
+ * _strstr - locates a substring.
+ * @needle: locate the first occurence of this substring.
+ * @haystack: locate substring in this string.
+ * Return: a pointer to the neginning of the located substring or,
+ *         NULL if the substring is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, found;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		found = 0;
+		for (j = 0; haystack[i + j] && needle[j]; j++)
+		{
+			if (haystack[i + j] == needle[j])
+			{
+				found = 1;
+				continue;
+			}
+			else
+			{
+				found = 0;
+				break;
+			}
+		}
+
+		if (found == 1)
+			return (haystack + i);
+	}
+
+	return (0);
+}
