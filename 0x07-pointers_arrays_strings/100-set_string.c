916b9c82c342be56e4e8bbf736b631ee84cb0b8c
Berehan <berehanf.hewot@gmail.com>
2023-08-01 05:19:37 +0300

    Create 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s: the pointer to set to char.
+ * @to: the value of the char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
