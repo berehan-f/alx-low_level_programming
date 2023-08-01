a1abd17d53abcc855d28873d50202037d4d72b3e
Berehan <berehanf.hewot@gmail.com>
2023-08-01 05:00:31 +0300

    Create 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * print_chessboard - prints a chessboard.
  * @a: the chessboard to print.
@@ -9,11 +11,9 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; a[i][7]; i++)
 	{
-		for (j = 0; j < 8; j++)	
+		for (j = 0; j < 8; j++)
 			_putchar(a[i][j]);
 
 		_putchar('\n');
 	}
 }
-									}
-	
