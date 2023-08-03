c65bcdb15e86d2c5b8273bf7eee73e6feb3fa00b
Berehan <berehanf.hewot@gmail.com>
2023-08-03 05:29:52 +0300

    Apply Betty coding style on 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,11 @@
 /**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: the number whose square root to compute.
- * Return: the natural square root of n.
+ * check - checks if a number has a square root.
+ * @n: the number to check.
+ * @x: check if x is the square root of n.
+ * Return: the square root of n, or -1 otherwise.
  */
-int check (int n, int x)
+
+int check(int n, int x)
 {
 	if (n < x)
 		return (-1);
@@ -11,9 +13,16 @@ int check (int n, int x)
 		return (x);
 	return (check(n, x + 1));
 }
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: the number whose square root to compute.
+ * Return: the natural square root of n.
+ */
+
 int _sqrt_recursion(int n)
 {
 	int result = check(n, 1);
+
 	return (result);
 }
-
