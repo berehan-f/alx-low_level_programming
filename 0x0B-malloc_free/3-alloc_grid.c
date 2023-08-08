74c971044c09717086af2aeee653220d6a9e7d30
Berehan <berehanf.hewot@gmail.com>
2023-08-08 10:02:32 -0400

    Update 3-alloc_grid.c, previously allocated memory is now freed if malloc fails

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@
 int **alloc_grid(int width, int height)
 {
 	int **grid;
-	int i, j;
+	int i, j, k;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
@@ -25,9 +25,13 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(int) * width);
 
 		if (grid[i] == NULL)
+		{
+			for (k = 0; k < i; k++)
+				free(grid[k]);
+			free(grid);
 			return (NULL);
+		}
 	}
-
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
