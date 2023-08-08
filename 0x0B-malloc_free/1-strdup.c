0b21da9f0b709cce1ef51d92baf4bbe613ca8df7
Berehan <berehanf.hewot@gmail.com>
2023-08-08 06:38:58 -0400

    Fix betty coding style in 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,7 +3,7 @@
 /**
  * _strdup - allocate a space in memory that contains,
  *           the copy of the string given as parameter.
- * @str - the string given as parameter.
+ * @str: the string given as parameter.
  * Return: a pointer to the newly allocated space.
  */
 
@@ -11,7 +11,7 @@ char *_strdup(char *str)
 {
 	char *copy;
 	int i, j;
-	
+
 	if (str == 0)
 		return (0);
 	for (i = 0; str[i]; i++)
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 	copy = malloc(sizeof(char) * i);
 	if (copy != 0 && i != 0)
 	{
-		for (j = 0; j < i; j++)
+		for (j = 0; j <= i; j++)
 			copy[j] = str[j];
 		return (copy);
 	}
