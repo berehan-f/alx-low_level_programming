fa28e67474bd8fbfa589d30de638ae74a054ff7b
Berehan <berehanf.hewot@gmail.com>
2024-01-15 03:09:00 -0500

    Code optimization for 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,12 +1,13 @@
 /**
- * _strlen_recursion - returns the length of a string.
- * @s: the string to compute the length of.
- * Return: the length of the string s.
+ * _strlen_recursion - calculates the length of the string pointed to by s.
+ * @s: the string length in question.
+ * Return: the number of bytes in the string pointed to by s.
  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s == 0)
 		return (0);
+
 	return (1 + _strlen_recursion(s + 1));
 }
