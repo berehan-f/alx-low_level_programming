3202b4912bafd9e7b3244e25aed9b140e5337032
Berehan <berehanf.hewot@gmail.com>
2024-01-16 19:29:56 -0500

    Create 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,12 +10,15 @@ char *_strdup(char *str)
 	int i;
 	char *s;
 
+	if (str == 0)
+		return (0);
+
 	for (i = 0; str[i]; i++)
 		;
 
 	s = malloc(sizeof(char) * (i + 1));
 
-	if (s != 0 && str != 0)
+	if (s != 0)
 	{
 		for (; i >= 0; i--)
 			s[i] = str[i];
