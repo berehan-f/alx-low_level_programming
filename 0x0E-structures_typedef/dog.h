f6f8f0bdaf90eea5d34ea2f0cf23e0d9c3cafede
Berehanfh <berehanf.hewot@gmail.com>
2024-01-22 22:21:03 +0300

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - describes a dog.
+ * @name: pet name
+ * @age: pet age
+ * @owner: owner name
+ *
+ * Description: describes attributes of a dog.
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+#endif
