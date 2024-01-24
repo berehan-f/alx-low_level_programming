b579513eee629f438381890fd15e9fa005872e47
Berehanfh <berehanf.hewot@gmail.com>
2024-01-24 14:58:01 +0300

    Create 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+/**
+ * print_name - prints a name.
+ * @name: name to print.
+ * @f: callback function that prints name in specific way.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
