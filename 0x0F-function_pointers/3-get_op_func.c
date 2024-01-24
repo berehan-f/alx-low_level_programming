2d854f73308dec7d60d32f057d4b0f507be6c56c
Berehanfh <berehanf.hewot@gmail.com>
2024-01-24 22:04:13 +0300

    Fix operator identification in 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,9 +18,9 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 
-	while (i < 5)
+	while (ops[i].op)
 	{
-		if (ops[i].op[0] == s[0])
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
