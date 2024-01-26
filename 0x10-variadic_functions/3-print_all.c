9f129612c8bc27bcb6fd0b9f2c553a20fe127a12
Berehanfh <berehanf.hewot@gmail.com>
2024-01-26 05:58:09 +0300

    Create 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,9 +7,10 @@
 
 void print_all(const char * const format, ...)
 {
-	va_list list;
-	char *s, *split;
 	int i = 0;
+	char *s, *sep = "";
+
+	va_list list;
 
 	va_start(list, format);
 
@@ -17,34 +18,32 @@ void print_all(const char * const format, ...)
 	{
 		while (format[i])
 		{
-			if (i == 0)
-				split = "";
-			if (i != 0)
-				split = ", ";
 			switch (format[i])
 			{
 				case 'c':
-					printf("%s%c", split, va_arg(list, int));
+					printf("%s%c", sep, va_arg(list, int));
 					break;
 				case 'i':
-					printf("%s%d", split, va_arg(list, int));
+					printf("%s%d", sep, va_arg(list, int));
 					break;
 				case 'f':
-					printf("%s%f", split, va_arg(list, double));
+					printf("%s%f", sep, va_arg(list, double));
 					break;
 				case 's':
 					s = va_arg(list, char *);
 					if (!s)
 						s = "(nil)";
-					printf("%s%s", split, s);
+					printf("%s%s", sep, s);
 					break;
 				default:
 					i++;
 					continue;
 			}
+			sep = ", ";
 			i++;
 		}
 	}
-	va_end(list);
+
 	printf("\n");
+	va_end(list);
 }
