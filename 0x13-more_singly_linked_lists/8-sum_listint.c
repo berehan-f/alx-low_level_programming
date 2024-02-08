6ddc7e4bd3c9876473e39ab76853cfdb676611f8
Berehanfh <berehanf.hewot@gmail.com>
2024-02-08 04:18:31 +0300

    Update 8-sum_listint.c, Fix betty coding style error.

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_listint - returns the sum of all the data(n) of 
+ * sum_listint - returns the sum of all the data(n) of
  *               a listint_t linked list.
  * @head: pointer to the first node of a listint_t linked list.
  * Return: sum
