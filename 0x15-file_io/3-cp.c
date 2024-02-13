63e8456603a3b097f693835ffbf7674d32adb235
Berehanfh <berehanf.hewot@gmail.com>
2024-02-13 05:44:23 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *create_buffer(char *file);
+void close_file(int fd);
+
 /**
  * close_file - closes opened file descriptors.
  * @f: the file to close.
