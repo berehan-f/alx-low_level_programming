1f42a9ab8ce57e6f803b0d7a9673c2ed0a48a014
Berehanfh <berehanf.hewot@gmail.com>
2024-02-13 05:41:42 +0300

    Create 3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,94 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * close_file - closes opened file descriptors.
+ * @f: the file to close.
+ */
+
+void close_file(int f)
+{
+	int x;
+
+	x = close(f);
+
+	if (x == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file %d\n", f);
+		exit(100);
+	}
+}
+
+/**
+ * create_buffer - creates a buffer of  size 1024 bytes.
+ * @filename: The name of the file to write to.
+ * Return: A pointer to the created buffer.
+ */
+
+char *create_buffer(char *filename)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * main - copies the content of a file to another file.
+ * @argc: the number of command line arguments.
+ * @argv: an array of command line arguments.
+ * Return: 0 on success.
+ */
+
+int main(int argc, char *argv[])
+{
+	int from, to, r, w;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+	free(buffer);
+	close_file(from);
+	close_file(to);
+	return (0);
+}
