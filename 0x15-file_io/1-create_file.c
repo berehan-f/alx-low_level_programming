75ca29507916e406129d9cfd69c3f34fe6b863b5
Berehanfh <berehanf.hewot@gmail.com>
2024-02-13 04:49:46 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,19 +9,19 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, str_len;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (str_len = 0; text_content[str_len]; str_len++)
-			;
+		while (text_content[len])
+			len++;
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, str_len);
+	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
